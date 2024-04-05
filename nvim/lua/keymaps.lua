ed6194c1aac1bfa303d54980263996d7fbecd7d4
Thijs Nijhof <thijs.nijhof@wrenkitchens.com>
2024-04-05 12:26:20 +0100

    init commit

---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,25 @@
+-- common options
+local options = {
+	noremap = true -- non-recursive
+	silent = false -- show message
+}
+
+-- ----------- --
+-- Normal Mode --
+-- ----------- --
+-- Window Navigation
+vim.keymap.set('n', '<C-h>', '<C-w>h', options)
+vim.kepmap.set('n', '<C-j>', '<C-w>j', options)
+vim.keymap.set('n', '<C-k>', '<C-w>k', options)
+vim.keymap.set('n', '<C-l>', '<C-w>l', options)
+-- Resizing
+vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', options)
+vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', options)
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', options)
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', options)
+
+-- ---------- --
+-- Visal mode --
+-- ---------- --
+vim.keymap.set('v', '<', '<gv', options)
+vim.keymap.set('n', '>', '>gv', options)
