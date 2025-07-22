187ead61338e0896989d7f0b458e5c6234ebc5b1
Thijs Nijhof <thijs.nijhof@wrenkitchens.com>
2025-07-22 13:59:50 +0100

    Add NVIM DAP

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,8 +15,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
-
 -- info: require where plugins > 1
 local git_related = require("plugins.git_related")
 local mason = require("plugins.mason")
@@ -31,11 +29,8 @@ require("lazy").setup({
 
     -- plugins
     require("plugins.lua_snip"),
-
     require("plugins.friendly_snippets"),
-
     require("plugins.nvim_tree"),
-
     require("plugins.autoclose_brackets"),
 
     git_related[1], -- vim fugitive
@@ -53,56 +48,64 @@ require("lazy").setup({
     },
 
     require("plugins.which_key"),
-
     require("plugins.git_signs"),
-
     require("plugins.lua_line"),
-
     require("plugins.comment"),
-
-    require("plugins.tree_sitter"),
-
     require("plugins.jester"),
-
     require("plugins.lsp_saga"),
-
     require("plugins.indent_line"),
-
     require("plugins.editor_config"),
-
     require("plugins.mardown_preview"),
-
     require("plugins.startup"),
-
     require("plugins.hlslens"),
-
     require("plugins.cursorline"),
-
     require("plugins.TODO_comments"),
-
     require("plugins.fidget"),
-
     require("plugins.neoclip"),
-
     require("plugins.scrollbar"),
-
     require("plugins.cheatsheet"),
-
     require("plugins.git_messenger"),
-
     require("plugins.smear_cursor"),
+    require("plugins.nvim_dap"),
+
+    -- https://github.com/mxsdev/nvim-dap-vscode-js
+    require("plugins.nvim_dap_vscode_js").setup({
+     -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+    debugger_path = "~/git/vscode-js-debug", -- Path to vscode-js-debug installation.
+     -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+     adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
+     -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
+     -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
+     -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+    }),
 
     telescope_setup,
     telescope_fzf_native_setup,
-    -- add more
 })
 
-
+for _, language in ipairs({ "typescript", "javascript" }) do
+  require("plugins.nvim_dap").configurations[language] = {
+        {
+            {
+                type = "pwa-node",
+                request = "launch",
+                name = "Launch file",
+                program = "${file}",
+                cwd = "${workspaceFolder}",
+            },
+            {
+                type = "pwa-node",
+                request = "attach",
+                name = "Attach",
+                processId = require'dap.utils'.pick_process,
+                cwd = "${workspaceFolder}",
+            }
+        }
+    }
+end
 
 -- info: general settings
 require('settings')
-
-
 
 -- info: keymaps setup
 require("keymaps.general_keymaps")
---
+++ nvim/lua/plugins/nvim_dap.lua
@@ -0,0 +1,5 @@
+return {
+    "mfussenegger/nvim-dap",
+    recommended = true,
+    desc = "Debugging support. Requires language specific adapters to be configured. (see lang extras)",
+}
