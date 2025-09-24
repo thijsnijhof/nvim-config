5c42e250e06863d1f867942114f9601a36e0ae4d
Thijs Nijhof <thijs.nijhof@rockartech.com>
2025-09-24 15:49:45 +0100

    Add lightmode to theme

--- nvim/lua/themes/gruvbox_baby.lua
+++ nvim/lua/themes/gruvbox_baby.lua
@@ -1,9 +1,26 @@
+local M = {}
+
+-- Function to toggle between light and dark mode
+function M.toggle_theme()
+    if vim.g.gruvbox_baby_background_color == 'light' then
+        vim.g.gruvbox_baby_background_color = 'dark'
+    else
+        vim.g.gruvbox_baby_background_color = 'light'
+    end
+    vim.cmd[[colorscheme gruvbox-baby]]
+end
+
+-- Set keymap to toggle theme
+vim.api.nvim_set_keymap('n', '<leader>tt', 
+    [[<cmd>lua require('themes.gruvbox_baby').toggle_theme()<CR>]], 
+    { noremap = true, silent = true, desc = 'Toggle between light and dark theme' })
+
 return {
     { 'luisiacc/gruvbox-baby',
         config = function()
-            vim.cmd[[autocmd VimEnter * colorscheme gruvbox-baby]]
-            vim.g.gruvbox_baby_background_color = 'dark'  -- Or 'dark'
+            vim.g.gruvbox_baby_background_color = 'dark'  -- Default to dark mode
             vim.g.gruvbox_baby_transparent_mode = true
+            vim.cmd[[colorscheme gruvbox-baby]]
         end
     },
 }
