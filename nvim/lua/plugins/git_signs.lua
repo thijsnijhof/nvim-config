71ccc20d905721b852396f139f1141d0fa576bf7
Thijs Nijhof <thijs.nijhof@rockartech.com>
2025-09-25 10:28:25 +0100

    Update git signs for inline blame

--- nvim/lua/plugins/git_signs.lua
+++ nvim/lua/plugins/git_signs.lua
@@ -12,6 +12,35 @@ return {
                 topdelete = { text = "‾" },
                 changedelete = { text = "~" },
             },
+            current_line_blame = true, -- Enable blame by default
+            current_line_blame_opts = {
+                virt_text = true,
+                virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+                delay = 500, -- Reduced from 1000ms to 500ms
+                ignore_whitespace = false,
+                virt_text_priority = 100,
+            },
+            on_attach = function(bufnr)
+                local gs = package.loaded.gitsigns
+                
+                -- Toggle inline blame (in case you want to turn it off)
+                vim.keymap.set('n', '<leader>gb', function()
+                    gs.toggle_current_line_blame()
+                end, { buffer = bufnr, desc = 'Toggle Git Blame' })
+                
+                -- Navigation between hunks
+                vim.keymap.set('n', ']c', function()
+                    if vim.wo.diff then return ']c' end
+                    vim.schedule(function() gs.next_hunk() end)
+                    return '<Ignore>'
+                end, { expr = true, buffer = bufnr, desc = 'Next Git Hunk' })
+                
+                vim.keymap.set('n', '[c', function()
+                    if vim.wo.diff then return '[c' end
+                    vim.schedule(function() gs.prev_hunk() end)
+                    return '<Ignore>'
+                end, { expr = true, buffer = bufnr, desc = 'Previous Git Hunk' })
+            end,
         },
     },
 }
